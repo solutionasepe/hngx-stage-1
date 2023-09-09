394993fc138685d57d7a9546d1e46a27bde993f8
solutionasepe <solutionasepe@gmail.com>
2023-09-09 23:52:31 +0100

    new changes applied

--- Api/urls.py
+++ Api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("my_views/", views.my_view, name="my_view")
+    path("", views.my_view, name="my_view")
 ]
--- intern_project/urls.py
+++ intern_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("Api.urls"))
+    path("api", include("Api.urls"))
 ]
